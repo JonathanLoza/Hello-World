5c013e4e31e471d1fa2c58b5e0d447f2b518e94e
JonathanLoza <lozajnt@gmail.com>
2019-03-28 12:31:01 -0500

    contar acabado

--- contar.cpp
+++ contar.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main() {
   string texto;
-  map<string,int> abecedario;
-  map<string,int>::iterator it;
+  map<char,int> abecedario;
+  map<char,int>::iterator it;
   getline(cin, texto);
   cout<<texto<<endl;
   for(int i=0; i<texto.length(); ++i){
     it=abecedario.find(texto[i]);
-    if (it != texto.end()){
-      abecedario[texto[i]]=+1;
+    if (it != abecedario.end()){
+      abecedario[texto[i]]+=1;
     }
     else{
       abecedario.emplace(texto[i],1);
@@ -20,7 +20,7 @@ int main() {
   }
 
   for(auto item:abecedario ){
-    cout<<item.first()<<": "<<item.second()<<endl;
+    cout<<item.first<<": "<<item.second<<endl;
   }
   return 0;
 }
